deb095cbd4563b84b56c80ff8860422e94339007
TNjikelana <tnjikela021@student.wethinkcode.co.za>
2024-11-16 16:51:48 +0200

    Added updates in few functions for starting with basic routing

---
+++ BackEnd/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def landing():
+    return render_template('index.html')  # Ensure your landing page is named 'index.html'
+
+@app.route('/upload') #link in js
+def upload():
+    return render_template('ImageAnalysis.html')
+
+@app.route('/analysis') #link in js
+def analysis():
+    return render_template('FarmingAnalysis.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
